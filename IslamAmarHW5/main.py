2b45e78bd8d90aecbeee8920b6a62832ade9c9ef
islam <161100@ppu.edu.ps>
2020-12-20 21:25:23 +0200

    Ploting the number of infections over several months in 3-4 European countries

--- IslamAmarHW5/main.py
+++ IslamAmarHW5/main.py
@@ -16,3 +16,25 @@ spain_x =[]
 spain_y =  []
 france_x = []
 france_y =  []
+countries =["Germany","Spain","France"]
+# Ploting the number of infections over several months in 3-4 European countries
+def ploting_data_set(dataset):
+
+        Geramnyset = (dataset[dataset["Country"] == "Germany"])
+        germany_x = pd.to_datetime(Geramnyset['Date'])
+        germany_y= pd.to_numeric(Geramnyset['Total Case'])
+
+        Spainset = (dataset[dataset["Country"] == "Spain"])
+        spain_x = pd.to_datetime(Spainset['Date'])
+        spain_y = pd.to_numeric(Spainset['Total Case'])
+
+        Franceset =(dataset[dataset["Country"] == "France"])
+        france_x = pd.to_datetime(Franceset['Date'])
+        france_y = pd.to_numeric(Franceset['Total Case'])
+
+        fig, ax = plt.subplots()  # one axis
+        ax.set(xlabel='Date', ylabel='Number of Positive Cases', title='COVID-19 total cases  in Germany,United Kingdom and Spain')
+        ax.plot(germany_x, germany_y, label="Germany")
+        ax.plot(france_x, france_y, label="France")
+        ax.plot(spain_x, spain_y, label="Spain")
+        ax.legend(loc='best')
